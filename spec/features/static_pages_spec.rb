06b5f633079951d03a34e6cdd3d730bf6c48335a
asao <asaoushine6@gmail.com>
2015-05-13 01:36:49 +0900

    finish static-pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -9,6 +9,13 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Rails Tutorial App | Home")
+    end
+
+
   end
 
   describe "Help page" do
@@ -16,12 +23,22 @@ describe "Static pages" do
   	  visit '/static_pages/help'
   	  expect(page).to have_content('Help')
   	end
+
+  	it "should have the title 'Help'" do
+  	  visit '/static_pages/help'
+  	  expect(page).to have_title("Ruby on Rails Tutorial Rails Tutorial App | Help")
+    end
   end
 
   describe "About page" do
   	it "shuold have the content 'About Us'" do
   	  visit '/static_pages/about'
   	  expect(page).to have_content('About Us')
+  	end
+
+  	it "should have the title 'About Us'" do
+  	  visit '/static_pages/about'
+  	  expect(page).to have_title("Ruby on Rails Tutorial Rails Tutorial App | About Us")
   	end
   end
 
